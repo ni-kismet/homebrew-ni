2ad8dc2b5a0c63b3c96c7058a04fffac5d0aab8b
Fred Visser <1458528+fredvisser@users.noreply.github.com>
2025-07-21 13:31:44 -0400

    slcli 0.1.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/slcli.rb
+++ Formula/slcli.rb
@@ -1,9 +1,9 @@
 class Slcli < Formula
   desc "SystemLink Integrator CLI: Manage SystemLink test plan templates and workflows"
   homepage "https://github.com/ni-kismet/systemlink-cli"
-  url "https://github.com/ni-kismet/systemlink-cli/releases/download/v0.1.1/slcli.tar.gz"
-  version "0.1.1"
-  sha256 "915f236b4f237ade67f44c7e45030330757906dd02f24cdc67adb7b30e10e037"
+  url "https://github.com/ni-kismet/systemlink-cli/releases/download/v0.1.2/slcli.tar.gz"
+  version "0.1.2"
+  sha256 "bc9b1668c3c2bc75a95f3d66691751621fa6cda9c7467a5f4780be678314c8ac"
   license "MIT"
 
   def install
